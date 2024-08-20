be84ce02576603676db9160b497bf32ddd8fe0a2
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-20 07:32:09 -0700

    planned my getComputerChoice function is pseudocode
    the idea is to use the math.random function to get a random number from 0 to less then 1
    
    i split the different choices by 0.33

--- js1.js
+++ js1.js
@@ -1,1 +1,8 @@
 console.log("Hello World!")
+
+/* a function named getComputereChoice that 
+randomly returnes 'rock', 'paper', or 'scissors' 
+
+if number is less than or equal to 0.33 return 'rock'
+else if number is greater than 0.33 and less than or equal to  0.66 return 'paper'
+else if number is greater then 0.66 return 'scissors' 
