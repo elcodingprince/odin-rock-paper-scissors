bfb7ef34dfba92c01bc9226e70673546e06e527c
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-20 15:18:50 -0700

    added conditions to playRound function
    if humans choices beats computers choices bases off the games rules then return console.log "you win, your choice beats computers choice"
    
    else essentially do the opposite
    
    also added a tie condition

--- js1.js
+++ js1.js
@@ -38,5 +38,11 @@ humanScore = 0;
 computerScore = 0;
 
 function playRound (humanChoice, computerChoice) {
-
+    if (humanChoice === computerChoice) {
+        return console.log("It's a tie!")
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+        return console.log("You Win! " + humanChoice + "beats " + computerChoice + ".")
+    } else {
+        return console.log("You Lose!" + computerChoice + "beats " + humanChoice + ".")
+    }
 }
