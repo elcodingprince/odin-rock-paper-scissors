3395c371f2da3b1224bf02a5c0db5fe4212dc890
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-20 07:34:27 -0700

    got stuck on how to write a function
    will ba back after looking it up on google, not chatgpt (;

--- js1.js
+++ js1.js
@@ -1,8 +1,10 @@
 console.log("Hello World!")
 
-/* a function named getComputereChoice that 
+/* a function named getComputerChoice that 
 randomly returnes 'rock', 'paper', or 'scissors' 
 
 if number is less than or equal to 0.33 return 'rock'
 else if number is greater than 0.33 and less than or equal to  0.66 return 'paper'
-else if number is greater then 0.66 return 'scissors' 
+else if number is greater then 0.66 return 'scissors' */
+
+const getComputerChoice = 
